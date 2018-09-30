e3a74bf662f3ee262c7102b47a53f04dea9997ca
Sidney Kingsley <sidney.kingsley@students.plymouth.ac.uk>
2018-09-30 16:41:28 +0100

    new

---
+++ Homework Week 1/FlexBox/js/index.js
@@ -0,0 +1,13 @@
+var value1;
+var value2;
+var value3;
+
+document.write(value1);
+document.write(value2);
+blep(value1, value2);
+document.write(value3);
+
+
+function blep(a, b){
+  a + b = value3;
+}
